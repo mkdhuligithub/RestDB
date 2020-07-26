255700d48997fd9b833a55a268da325f5af2aa21
mkdhuligithub <mkdhuli@yahoo.com>
2020-07-26 15:29:00 -0400

    Added test command line shell scripts for curl commands

---
+++ src/test/post.sh
@@ -0,0 +1,1 @@
+curl -X POST -d '{"accountId":"A12","accountName":"Account 12"}' -H 'Content-Type: application/json' http://localhost:8080/accounts
---
+++ src/test/put.sh
@@ -0,0 +1,1 @@
+curl -X PUT -d '{"accountId":"A10","accountName":"Dummy Account 10"}' -H 'Content-Type: application/json' http://localhost:8080/accounts/A10
