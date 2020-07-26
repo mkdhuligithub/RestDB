fd5e6b280c8fc52f36d8ea1c361b10360ec153ab
mkdhuligithub <mkdhuli@yahoo.com>
2020-07-26 15:13:31 -0400

    Added CREATE operation for CRUD

--- src/main/java/com/example/RestDB/AccountController.java
+++ src/main/java/com/example/RestDB/AccountController.java
@@ -1,14 +1,12 @@
 package com.example.RestDB;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
-//import javax.validation.Valid;
+import javax.validation.Valid;
 
 //@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 @RestController
@@ -16,13 +14,18 @@ public class AccountController {
     @Autowired
     AccountRepository accountRepository;
 
-    @GetMapping("/accounts")
+    @GetMapping("/accounts") // READ operation in CRUD
     public List<Account> getAllAccounts() {
         return accountRepository.findAll();
     }
 
-    @GetMapping("/accounts/{accountId}")
+    @GetMapping("/accounts/{accountId}") // READ operation in CRUD
     public Optional<Account> getAllAccountById(@PathVariable(value="accountId") String accountId) {
         return accountRepository.findById(accountId);
+    }
+
+    @PostMapping("/accounts") // CREATE operation in CRUD
+    public @Valid Account createEmployee(@Valid @RequestBody Account account) {
+        return accountRepository.save(account);
     }
 }
